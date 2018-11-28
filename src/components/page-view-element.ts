ec8b93f359761e7c17cb0e158b194c7f0fcd92f6
Giorgio Garasto <giorgio@garasto.it>
2018-11-28 08:34:59 +0100

    feature: Implement page-view-element

---
+++ src/components/page-view-element.ts
@@ -0,0 +1,11 @@
+import { LitElement, property } from '@polymer/lit-element';
+
+export class PageViewElement extends LitElement {
+  @property({type: Boolean})
+  protected active = false;
+
+  // Only render this page if it's actually visible.
+  protected shouldUpdate() {
+    return this.active;
+  }
+}
