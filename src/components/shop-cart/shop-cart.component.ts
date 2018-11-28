d2d0aabfb679848e458d029514268c5c978329df
Giorgio Garasto <giorgio@garasto.it>
2018-11-28 21:00:37 +0100

    feature: Implement shop-cart

---
+++ src/components/shop-cart/shop-cart.component.ts
@@ -0,0 +1,38 @@
+import { LitElement, property } from '@polymer/lit-element';
+import { connect } from 'pwa-helpers/connect-mixin';
+
+// This element is connected to the Redux store.
+import { RootState, store } from '@store';
+
+// These are the actions needed by this element.
+import { removeFromCart } from '@actions/shop';
+
+// These are the reducers needed by this element.
+import { cartItemsSelector, cartTotalSelector } from '@reducers/shop';
+import { CartItem } from '@reducers/shop';
+
+import template from './shop-cart.template';
+
+export class ShopCart extends connect(store)(LitElement) {
+  @property({type: Array})
+  protected _items: CartItem[] = [];
+
+  @property({type: Number})
+  protected _total = 0;
+
+  // This is called every time something is updated in the store.
+  public stateChanged(state: RootState) {
+    this._items = cartItemsSelector(state);
+    this._total = cartTotalSelector(state);
+  }
+
+  protected render() {
+    return template.call(this);
+  }
+
+  protected _removeButtonClicked(e: Event) {
+    store.dispatch(removeFromCart((e.currentTarget as HTMLButtonElement).dataset.index));
+  }
+}
+
+window.customElements.define('shop-cart', ShopCart);
---
+++ src/components/shop-cart/shop-cart.template.ts
@@ -0,0 +1,37 @@
+import { html } from '@polymer/lit-element';
+import { ShopCart } from './shop-cart.component';
+
+import styles from './shop-cart.styles';
+
+// These are the elements needed by this element.
+import { removeFromCartIcon } from '@components/my-icons';
+import '@components/shop-item/shop-item.component';
+
+// These are the shared styles needed by this element.
+import buttonsSharedStyles from '@components/buttons-shared.styles';
+
+export default function template(this: ShopCart) {
+  return html`
+    ${buttonsSharedStyles}
+    ${styles}
+
+    <p ?hidden="${this._items.length !== 0}">Please add some products to cart.</p>
+      ${this._items.map((item) =>
+        html`
+          <div>
+            <shop-item
+              .name="${item.title}"
+              .amount="${item.amount}"
+              .price="${item.price}">
+            </shop-item>
+            <button
+              @click="${this._removeButtonClicked}"
+              data-index="${item.id}"
+              title="Remove from cart">
+              ${removeFromCartIcon}
+            </button>
+          </div>
+      `)}
+    <p ?hidden="${!this._items.length}"><b>Total:</b> ${this._total}</p>
+  `;
+}
