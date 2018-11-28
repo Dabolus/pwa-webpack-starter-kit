e57280fdf5d165ad1f07c6ba62a80ed68b84b088
Giorgio Garasto <giorgio@garasto.it>
2018-11-28 20:26:17 +0100

    fix: Load snack bar styles

--- src/components/snack-bar/snack-bar.template.ts
+++ src/components/snack-bar/snack-bar.template.ts
@@ -1,6 +1,12 @@
 import { html } from '@polymer/lit-element';
 import { SnackBar } from './snack-bar.component';
 
+import styles from './snack-bar.styles';
+
 export default function template(this: SnackBar) {
-  return html`<slot></slot>`;
+  return html`
+    ${styles}
+
+    <slot></slot>
+  `;
 }
