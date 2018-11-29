fd6f89dfc2769b9003a90cc402af056d4f8bb578
Giorgio Garasto <giorgio@garasto.it>
2018-11-29 21:08:00 +0100

    refactor: Extract Babel config so that it can be used elsewhere

---
+++ babel.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  presets: [
+    ['@babel/env', {
+      loose: true,
+      useBuiltIns: 'usage',
+      ...process.env.NODE_ENV === 'test' ? {} : {
+        modules: false,
+      },
+    }],
+    '@babel/typescript',
+  ],
+  plugins: [
+    ['template-html-minifier', {
+      modules: {
+        'lit-html': ['html', 'svg'], // lit-html
+        '@polymer/lit-element': ['html', 'svg'], // LitElement
+        '@polymer/polymer/polymer-element': ['html'], // Polymer 3 - exported from Polymer Element
+        '@polymer/polymer/lib/utils/html-tag.js': ['html'], // Polymer 3 - exported from utils (used by PolymerElements family)
+      },
+      htmlMinifier: {
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+      },
+    }],
+    ['@babel/transform-runtime', {
+      corejs: 2,
+      sourceType: 'unambiguous',
+    }],
+    ['@babel/proposal-decorators', {
+      decoratorsBeforeExport: true,
+    }],
+    ['@babel/proposal-class-properties', {
+      loose: true,
+    }],
+    '@babel/syntax-dynamic-import',
+  ],
+};
