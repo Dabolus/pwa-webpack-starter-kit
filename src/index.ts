7a115a9e906c2c69b87a8c3bd9572d8b2bfc0dbc
Giorgio Garasto <giorgio@garasto.it>
2019-01-21 08:00:54 +0100

    refactor: Decrease cognitive complexity

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,36 @@
 // Feature detect which polyfill needs to be imported.
-const needsTemplate = (() => {
-  // no real <template> because no `content` property (IE and older browsers)
-  const t: HTMLTemplateElement = document.createElement('template');
-  if (!('content' in t)) {
-    return true;
-  }
-  // broken doc fragment (older Edge)
-  if (!(t.content.cloneNode() instanceof DocumentFragment)) {
-    return true;
-  }
-  // broken <template> cloning (Edge up to at least version 17)
-  const t2: HTMLTemplateElement = document.createElement('template');
-  t2.content.appendChild(document.createElement('div'));
-  t.content.appendChild(t2);
-  const clone: any = t.cloneNode(true);
-  return (clone.content.childNodes.length === 0 ||
-    clone.content.firstChild.content.childNodes.length === 0);
-})();
 
-const polyfills = (() => {
-  // NOTE: any browser that does not have template or ES6 features
-  // must load the full suite of polyfills.
-  if (!Promise || !Array.from || !window.URL || !Symbol || needsTemplate) {
-    return ['sd-ce-pf'];
-  }
-  const neededPolyfills = [];
-  if (!('attachShadow' in Element.prototype && 'getRootNode' in Element.prototype) ||
-    ((window as any).ShadyDOM && (window as any).ShadyDOM.force)) {
-    neededPolyfills.push('sd');
-  }
-  if (!window.customElements || (window.customElements as any).forcePolyfill) {
-    neededPolyfills.push('ce');
-  }
-  return neededPolyfills;
-})();
+let needsTemplate = false;
+// no real <template> because no `content` property (IE and older browsers)
+const t: HTMLTemplateElement = document.createElement('template');
+if (!('content' in t)) {
+  needsTemplate = true;
+}
+// broken doc fragment (older Edge)
+if (!(t.content.cloneNode() instanceof DocumentFragment)) {
+  needsTemplate = true;
+}
+// broken <template> cloning (Edge up to at least version 17)
+const t2: HTMLTemplateElement = document.createElement('template');
+t2.content.appendChild(document.createElement('div'));
+t.content.appendChild(t2);
+const clone: any = t.cloneNode(true);
+needsTemplate = (clone.content.childNodes.length === 0 ||
+  clone.content.firstChild.content.childNodes.length === 0);
+
+let polyfills: string[] = [];
+// NOTE: any browser that does not have template or ES6 features
+// must load the full suite of polyfills.
+if (!Promise || !Array.from || !window.URL || !Symbol || needsTemplate) {
+  polyfills = ['sd-ce-pf'];
+}
+if (!('attachShadow' in Element.prototype && 'getRootNode' in Element.prototype) ||
+  ((window as any).ShadyDOM && (window as any).ShadyDOM.force)) {
+  polyfills.push('sd');
+}
+if (!window.customElements || (window.customElements as any).forcePolyfill) {
+  polyfills.push('ce');
+}
 
 // Note that in this case we need to append the .js extension, otherwise
 // Webpack will try to load the .js.map files into the bundle too.
